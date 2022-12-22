0753ff979dec79eb7e52450a97b93888001a0d01
salmonte91 <stevenalmonte4@gmail.com>
2022-12-21 20:52:03 -0500

    added questions

---
+++ index.js
@@ -0,0 +1,53 @@
+let questions = [
+    {
+        q : "In what type of matter are atoms most tightly packed?",
+        o : [
+            "Liquids",
+            "Solids",
+            "Gases",
+            "Plasma"
+        ],
+        a : 1 // Solids is the answer
+    },
+    {
+        q : "What is the hottest planet in the solar system?",
+        o : [
+            "Venus",
+            "Mercury",
+            "Mars",
+            "Sun"
+        ],
+        a : 0 // Venus is the answer
+    },
+    {
+        q : "What is the opposite of matter?",
+        o : [
+            "Mass",
+            "Gravity",
+            "Space",
+            "Antimatter"
+        ],
+        a : 3 // Antimatter is the answer
+    },
+    {
+        q : "What is the nearest planet to the sun?",
+        o : [
+            "Venus",
+            "Mercury",
+            "Mars",
+            "Earth"
+        ],
+        a : 2 // Mercury is the answer
+    },
+    {
+        q : "What tissues connect the muscles to the bones?",
+        o : [
+            "Bone Marrow",
+            "Cartilage",
+            "Ligaments",
+            "Tendons"
+        ],
+        a : 3
+    },
+
+]
